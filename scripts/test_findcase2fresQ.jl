47deeb3a7c9739c8216c66fd388ed5d5dbc9090f
Peter Simon <psimon0420@gmail.com>
2025-06-01 06:20:17 -0700

    Add start of sensitivity calculations to case2

--- scripts/test_findcase2fresQ.jl
+++ scripts/test_findcase2fresQ.jl
@@ -44,4 +44,20 @@ println()
 println("Estimated dielectric properties produce these errors:")
 @show (fresgoal, case2.fres - fresgoal)
 @show (Qgoal, case2.Q - Qgoal)
+
+# Examine sensitivity of ϵᵣ and tanδ to changes in the resonant frequency and Q of the second cavity
+using FiniteDifferences: grad, central_fdm
+fun_eps_f(f) = Cav.findet(geom, σ, f, Qgoal).ϵᵣ
+fun_eps_Q(Q) = Cav.findet(geom, σ, fresgoal, Q).ϵᵣ
+∂ϵᵣ∂f = central_fdm(11, 1)(fun_eps_f, fresgoal)[1] # -88.73020968895703
+∂ϵᵣ∂Q = central_fdm(11, 1; factor=2e18)(fun_eps_Q, Qgoal)[1] # -3.475112954414834e-6
+
+fun_tand_f(f) = Cav.findet(geom, σ, f, Qgoal).tanδ
+fun_tand_Q(Q) = Cav.findet(geom, σ, fresgoal, Q).tanδ
+∂tanδ∂f = central_fdm(3, 1; factor=1e12)(fun_tand_f, fresgoal)[1] # 0.1457377324694493
+∂tanδ∂Q = central_fdm(11, 1; factor=1e17)(fun_tand_Q, Qgoal)[1] # -4.3673063289868904e-5
+
+s = """[∂ϵᵣ/∂f      ∂ϵᵣ/∂Q  
+         ∂tanδ/∂f   ∂tanδ/∂Q ]"""
+println(s)
 nothing
